4f2430f671b196ce4f206c7377123d62d46344f5
Gonzalo <kronleuchter85@gmail.com>
2025-02-23 13:18:16 +0000

    terraform y documentacion

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # ceiot_proyecto_especializacion
 
+==========================================================
+AWS services
+==========================================================
+- AWS IoT Core
+    - Topic MQTT + Certs
+    - Rules para routera mensajes
+- AWS S3
+    - Para recibir los mensajes routeados desde MQTT
+    - Para storage data temporal Athena
+    - Para storage de terraform
+- AWS Glue Catalog
+    - Database
+    - Table de mensajes MQTT
+- AWS App Runner: 
+    - Para dApp
+- AWS Dynamo:
+    - Collections:
+        - ABI
+        - Expenses
+        - Contracts Registry
+- AWS Systems Manager:
+     - Parameters store:
+        - Alchemy project id
+        - Wallet account address
+        - Blockchain URLs
+- AWS Secret Manager: 
+    - Secret: Para key del wallet
+
+
+==========================================================
+Blockchain services
+==========================================================
+- Ethereum/Sepolia: no setup
+- Etherscan: no setup
+- Chainlist: no setup
+- Alchemy:
+    - Proyecto: Para el acceso a redes Ethereum
+- Metamask:
+    - Wallet + Account: Para fondear operaciones
+- Google Blockchain Faucets
+
+
+==========================================================
+Dev tools
+==========================================================
+- Truffle
+- Solidity
+- Docker + Docker Compose
+- Ganache
+- Node.js
+- Web3.js
+- ESP-IDF
+
+
+==========================================================
+Actividades
+==========================================================
+- Configuracion de credenciales AWS en la maquina local.
+- Creacion de componentes AWS
+- Modificacion de codigo ESP32 para incluir conexion segura MQTT
+- Despliegue de certificados MQTT de AWS IoT Core en ESP32
+- Configuracion de AWS Secret (private key) para escritura blockchain
+- Despliegue de firmware ESP32
+
+- Despliegue de Smart Contracts y dApp
+- Despliegue de data pipeline
+- Ejecucion del contenedor con terraform montando credenciales AWS
--- pocs/poc-aws-setup/terraform/main.tf
+++ pocs/poc-aws-setup/terraform/main.tf
@@ -1,7 +1,101 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "iot_data_bucket" {
-  bucket = "iot-data-bucket-1490i109id0adaa"
+
+#
+# S3 
+#
+variable "s3_bucket_name" {
+  default = "ceiot-exploratory-robot"
+}
+variable "s3_data_prefix" {
+  default = "readings"  
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+# resource "aws_s3_bucket" "iot_data_bucket" {
+#   bucket = "iot-data-bucket-${random_id.bucket_id.hex}"
+# }
+
+
+#
+# Glue
+#
+resource "aws_glue_catalog_database" "glue_database" {
+  name = "ceit_landing"
+}
+
+resource "aws_glue_catalog_table" "landing_readings_table" {
+  name          = "readings"
+  database_name = aws_glue_catalog_database.glue_database.name
+
+  table_type    = "EXTERNAL_TABLE"
+
+  parameters = {
+    "classification" = "json"
+    "EXTERNAL"       = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${var.s3_bucket_name}/${var.s3_data_prefix}"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "JsonSerDe"
+      serialization_library = "org.apache.hive.hcatalog.data.JsonSerDe"
+    }
+
+    columns {
+      name = "deviceId"
+      type = "string"
+    }
+
+    columns {
+      name = "type"
+      type = "string"
+    }
+
+    columns {
+      name = "value"
+      type = "string"
+    }
+
+    columns {
+      name = "location"
+      type = "string"
+    }
+
+    columns {
+      name = "time"
+      type = "string"
+    }
+  }
+}
+
+
+#
+# Secret
+#
+
+variable "wallet_private_key" {
+  description = "Private key de la wallet blockchain"
+  type        = string
+  sensitive   = true
+}
+
+resource "aws_secretsmanager_secret" "wallet_secret" {
+  name        = "wallet-private-key"
+  description = "Private key de la wallet blockchain"
+}
+
+resource "aws_secretsmanager_secret_version" "wallet_secret_version" {
+  secret_id     = aws_secretsmanager_secret.wallet_secret.id
+  secret_string = jsonencode({
+    private_key = var.wallet_private_key
+  })
+
 }
